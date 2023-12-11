be9a501dd47c38b40f92da30ec3df8a936035c62
Hugveldt <hugo.matthews@googlemail.com>
2023-12-11 15:43:11 +0000

    Added multiple position update passes for stability
    + rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::time::Duration;
 
-use egui::{Color32, Vec2, Stroke, Pos2};
 use eframe::egui;
+use egui::{Color32, Pos2, Stroke, Vec2};
 
-const TENSION: f32 = 50.0;
+const TENSION: f32 = 500.0;
 const TARGET_DISTANCE: f32 = 30.0;
 const TARGET_LEEWAY: f32 = 0.8;
 const EPSILON: f32 = 0.00001;
 const KNOT_RADIUS: f32 = 10.0;
 const ROPE_THICKNESS: f32 = 7.0;
+const NUM_POSITION_PASSES: usize = 20;
 
 fn main() {
     let window_size = Vec2::new(1280.0, 760.0);
@@ -34,7 +35,7 @@ fn main() {
     netsim.add_knot(center + Vec2::new(0.0, 300.0));
     netsim.add_knot(center + Vec2::new(0.0, 330.0));
 
-    netsim.add_knot(center + Vec2::new(30.0, 0.0));
+    /*netsim.add_knot(center + Vec2::new(30.0, 0.0));
     netsim.add_knot(center + Vec2::new(30.0, 30.0));
     netsim.add_knot(center + Vec2::new(30.0, 60.0));
     netsim.add_knot(center + Vec2::new(30.0, 90.0));
@@ -58,43 +59,39 @@ fn main() {
     netsim.add_knot(center + Vec2::new(60.0, 240.0));
     netsim.add_knot(center + Vec2::new(60.0, 270.0));
     netsim.add_knot(center + Vec2::new(60.0, 300.0));
-    netsim.add_knot(center + Vec2::new(60.0, 330.0));
+    netsim.add_knot(center + Vec2::new(60.0, 330.0));*/
 
     let num_knots = netsim.knots.len();
-    let num_columns = 3;
+    let num_columns = 1;
     let column_length = num_knots / num_columns;
-    
+
     for c in 0..num_columns {
-        let first_knot = c*column_length;
+        let first_knot = c * column_length;
         netsim.knots[first_knot].add_neighbour(first_knot + 1);
-        for i in 1..column_length-1 {
-            let this_knot = c*column_length + i;
-            netsim.knots[this_knot].add_neighbour(this_knot-1);
-            netsim.knots[this_knot].add_neighbour(this_knot+1);
+        for i in 1..column_length - 1 {
+            let this_knot = c * column_length + i;
+            netsim.knots[this_knot].add_neighbour(this_knot - 1);
+            netsim.knots[this_knot].add_neighbour(this_knot + 1);
         }
-        let last_knot = (c+1)*column_length - 1;
+        let last_knot = (c + 1) * column_length - 1;
         netsim.knots[last_knot].add_neighbour(last_knot - 1);
     }
 
-    for c in 0..num_columns-1 {
+    for c in 0..num_columns - 1 {
         for i in (0..column_length).step_by(4) {
-            let this_knot = c*column_length + i;
-            netsim.knots[this_knot].add_neighbour(this_knot+column_length);
+            let this_knot = c * column_length + i;
+            netsim.knots[this_knot].add_neighbour(this_knot + column_length);
         }
     }
 
     for c in 1..num_columns {
         for i in (0..column_length).step_by(4) {
-            let this_knot = c*column_length + i;
-            netsim.knots[this_knot].add_neighbour(this_knot-column_length);
+            let this_knot = c * column_length + i;
+            netsim.knots[this_knot].add_neighbour(this_knot - column_length);
         }
     }
 
-    eframe::run_native(
-        "Net Simulator",
-        options,
-        Box::new(|_cc| Box::new(netsim)),
-    );
+    eframe::run_native("Net Simulator", options, Box::new(|_cc| Box::new(netsim)));
 }
 
 #[derive(Debug)]
@@ -113,7 +110,7 @@ impl From<Vec2> for Knot {
     fn from(value: Vec2) -> Self {
         Knot {
             position: value,
-            neighbours: vec![]
+            neighbours: vec![],
         }
     }
 }
@@ -143,7 +140,11 @@ impl MyApp {
             let neighbour_position = self.knots[*neighbour].position;
 
             let difference = knot_position - neighbour_position;
-            let direction = if difference.length() > EPSILON { difference.normalized() } else { Vec2::new(0.0, 0.0) };
+            let direction = if difference.length() > EPSILON {
+                difference.normalized()
+            } else {
+                Vec2::new(0.0, 0.0)
+            };
 
             let target = neighbour_position + direction * TARGET_DISTANCE;
 
@@ -178,25 +179,41 @@ impl eframe::App for MyApp {
                 }
             }
 
-            for i in 0..self.knots.len() {
-                let velocity = self.calculate_velocity_for(i);
-                self.knots[i].position += velocity * TENSION * ui.input().stable_dt.min(0.1);
+            for _ in 0..NUM_POSITION_PASSES {
+                for i in 0..self.knots.len() {
+                    let velocity = self.calculate_velocity_for(i);
+                    let dt_per_pass = ui.input().stable_dt.min(0.1) / NUM_POSITION_PASSES as f32;
+                    self.knots[i].position += velocity * TENSION * dt_per_pass;
 
-                if let Some(dragged) = self.dragging {
-                    if dragged == i {
-                        self.knots[i].position = mouse.to_vec2();
+                    if let Some(dragged) = self.dragging {
+                        if dragged == i {
+                            self.knots[i].position = mouse.to_vec2();
+                        }
                     }
                 }
             }
 
             for i in 1..self.knots.len() {
                 for n in &self.knots[i].neighbours {
-                    painter.line_segment([self.knots[i].position.to_pos2(), self.knots[*n].position.to_pos2()], Stroke { width: ROPE_THICKNESS, color: Color32::GRAY });
+                    painter.line_segment(
+                        [
+                            self.knots[i].position.to_pos2(),
+                            self.knots[*n].position.to_pos2(),
+                        ],
+                        Stroke {
+                            width: ROPE_THICKNESS,
+                            color: Color32::GRAY,
+                        },
+                    );
                 }
             }
 
             for i in 0..self.knots.len() {
-                painter.circle_filled(self.knots[i].position.to_pos2(), KNOT_RADIUS, Color32::from_rgb(255/(i+1) as u8, 127, (i+1) as u8));
+                painter.circle_filled(
+                    self.knots[i].position.to_pos2(),
+                    KNOT_RADIUS,
+                    Color32::from_rgb(255 / (i + 1) as u8, 127, (i + 1) as u8),
+                );
             }
         });
 
