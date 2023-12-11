1a6a19aaf20353c63c8b08c58f1af4489ec0ec05
Hugveldt <hugo.matthews@googlemail.com>
2023-12-11 17:50:21 +0000

    rewrite + added collidable circles

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use std::time::Duration;
 
 use eframe::egui;
-use egui::{Color32, Pos2, Stroke, Vec2};
+use egui::{Color32, Pos2, Stroke, Vec2, Painter};
 
-const TENSION: f32 = 500.0;
-const TARGET_DISTANCE: f32 = 30.0;
-const TARGET_LEEWAY: f32 = 0.8;
+const TARGET_DISTANCE: f32 = 10.0;
 const EPSILON: f32 = 0.00001;
-const KNOT_RADIUS: f32 = 10.0;
-const ROPE_THICKNESS: f32 = 7.0;
-const NUM_POSITION_PASSES: usize = 20;
+const NUM_KNOTS: usize = 64;
+const KNOT_RADIUS: f32 = 3.0;
+const ROPE_THICKNESS: f32 = 6.0;
 
 fn main() {
     let window_size = Vec2::new(1280.0, 760.0);
@@ -22,138 +20,111 @@ fn main() {
     let mut netsim = MyApp::new();
 
     let center = Vec2::new(window_size.x / 2.0, window_size.y / 2.0);
-    netsim.add_knot(center + Vec2::new(0.0, 0.0));
-    netsim.add_knot(center + Vec2::new(0.0, 30.0));
-    netsim.add_knot(center + Vec2::new(0.0, 60.0));
-    netsim.add_knot(center + Vec2::new(0.0, 90.0));
-    netsim.add_knot(center + Vec2::new(0.0, 120.0));
-    netsim.add_knot(center + Vec2::new(0.0, 150.0));
-    netsim.add_knot(center + Vec2::new(0.0, 180.0));
-    netsim.add_knot(center + Vec2::new(0.0, 210.0));
-    netsim.add_knot(center + Vec2::new(0.0, 240.0));
-    netsim.add_knot(center + Vec2::new(0.0, 270.0));
-    netsim.add_knot(center + Vec2::new(0.0, 300.0));
-    netsim.add_knot(center + Vec2::new(0.0, 330.0));
 
-    /*netsim.add_knot(center + Vec2::new(30.0, 0.0));
-    netsim.add_knot(center + Vec2::new(30.0, 30.0));
-    netsim.add_knot(center + Vec2::new(30.0, 60.0));
-    netsim.add_knot(center + Vec2::new(30.0, 90.0));
-    netsim.add_knot(center + Vec2::new(30.0, 120.0));
-    netsim.add_knot(center + Vec2::new(30.0, 150.0));
-    netsim.add_knot(center + Vec2::new(30.0, 180.0));
-    netsim.add_knot(center + Vec2::new(30.0, 210.0));
-    netsim.add_knot(center + Vec2::new(30.0, 240.0));
-    netsim.add_knot(center + Vec2::new(30.0, 270.0));
-    netsim.add_knot(center + Vec2::new(30.0, 300.0));
-    netsim.add_knot(center + Vec2::new(30.0, 330.0));
-
-    netsim.add_knot(center + Vec2::new(60.0, 0.0));
-    netsim.add_knot(center + Vec2::new(60.0, 30.0));
-    netsim.add_knot(center + Vec2::new(60.0, 60.0));
-    netsim.add_knot(center + Vec2::new(60.0, 90.0));
-    netsim.add_knot(center + Vec2::new(60.0, 120.0));
-    netsim.add_knot(center + Vec2::new(60.0, 150.0));
-    netsim.add_knot(center + Vec2::new(60.0, 180.0));
-    netsim.add_knot(center + Vec2::new(60.0, 210.0));
-    netsim.add_knot(center + Vec2::new(60.0, 240.0));
-    netsim.add_knot(center + Vec2::new(60.0, 270.0));
-    netsim.add_knot(center + Vec2::new(60.0, 300.0));
-    netsim.add_knot(center + Vec2::new(60.0, 330.0));*/
-
-    let num_knots = netsim.knots.len();
-    let num_columns = 1;
-    let column_length = num_knots / num_columns;
-
-    for c in 0..num_columns {
-        let first_knot = c * column_length;
-        netsim.knots[first_knot].add_neighbour(first_knot + 1);
-        for i in 1..column_length - 1 {
-            let this_knot = c * column_length + i;
-            netsim.knots[this_knot].add_neighbour(this_knot - 1);
-            netsim.knots[this_knot].add_neighbour(this_knot + 1);
-        }
-        let last_knot = (c + 1) * column_length - 1;
-        netsim.knots[last_knot].add_neighbour(last_knot - 1);
+    for _ in 0..NUM_KNOTS {
+        netsim.add_knot(Knot { position: center });
     }
 
-    for c in 0..num_columns - 1 {
-        for i in (0..column_length).step_by(4) {
-            let this_knot = c * column_length + i;
-            netsim.knots[this_knot].add_neighbour(this_knot + column_length);
-        }
-    }
-
-    for c in 1..num_columns {
-        for i in (0..column_length).step_by(4) {
-            let this_knot = c * column_length + i;
-            netsim.knots[this_knot].add_neighbour(this_knot - column_length);
-        }
-    }
+    netsim.add_collider(Collidable { position: center, radius: 20.0 });
+    netsim.add_collider(Collidable { position: center + Vec2::new(200.0, 0.0), radius: 20.0 });
+    netsim.add_collider(Collidable { position: center + Vec2::new(-200.0, 0.0), radius: 20.0 });
+    netsim.add_collider(Collidable { position: center + Vec2::new(0.0, 200.0), radius: 20.0 });
 
     eframe::run_native("Net Simulator", options, Box::new(|_cc| Box::new(netsim)));
 }
 
-#[derive(Debug)]
+#[derive(Clone, Copy)]
 struct Knot {
     position: Vec2,
-    neighbours: Vec<usize>,
 }
 
-impl Knot {
-    fn add_neighbour(&mut self, other: usize) {
-        self.neighbours.push(other);
-    }
-}
-
-impl From<Vec2> for Knot {
-    fn from(value: Vec2) -> Self {
-        Knot {
-            position: value,
-            neighbours: vec![],
-        }
-    }
+struct Collidable {
+    position: Vec2,
+    radius: f32,
 }
 
 struct MyApp {
     knots: Vec<Knot>,
-    dragging: Option<usize>,
+    colliders: Vec<Collidable>,
 }
 
 impl MyApp {
     fn new() -> Self {
         MyApp {
             knots: vec![],
-            dragging: None,
+            colliders: vec![],
         }
     }
 
-    fn add_knot(&mut self, other: Vec2) {
-        self.knots.push(Knot::try_from(other).unwrap());
+    fn add_knot(&mut self, knot: Knot) {
+        self.knots.push(knot);
     }
 
-    fn calculate_velocity_for(&self, index: usize) -> Vec2 {
-        let mut velocity = Vec2::new(0.0, 0.0);
+    fn add_collider(&mut self, collider: Collidable) {
+        self.colliders.push(collider);
+    }
 
-        for neighbour in &self.knots[index].neighbours {
-            let knot_position = self.knots[index].position;
-            let neighbour_position = self.knots[*neighbour].position;
+    fn satisfy_constraints(&mut self) {
+        for i in 1..self.knots.len() {
+            let mut current_knot = self.knots[i];
+            let prev_knot = self.knots[i-1];
 
-            let difference = knot_position - neighbour_position;
-            let direction = if difference.length() > EPSILON {
-                difference.normalized()
-            } else {
-                Vec2::new(0.0, 0.0)
-            };
+            let difference = prev_knot.position - current_knot.position;
+            let current_distance = difference.length();
 
-            let target = neighbour_position + direction * TARGET_DISTANCE;
+            if (TARGET_DISTANCE - current_distance).abs() > EPSILON {
+                let adjustment = difference.normalized() * (TARGET_DISTANCE - current_distance);
 
-            if (target - knot_position).length() > TARGET_LEEWAY {
-                velocity += target - knot_position;
+                current_knot.position -= adjustment;
+
+                self.knots[i] = current_knot;
             }
         }
 
-        velocity
+        for knot in self.knots.iter_mut() {
+            for collider in self.colliders.iter() {
+                let difference = knot.position - collider.position;
+                let distance = difference.length();
+                if distance < collider.radius {
+                    knot.position = collider.position + difference.normalized() * collider.radius;
+                }
+            }
+        }
+    }
+
+    fn paint_colliders(&self, painter: &Painter) {
+        for collider in &self.colliders {
+            painter.circle_filled(collider.position.to_pos2(), collider.radius, Color32::RED);
+        }
+    }
+
+    fn paint_knots(&self, painter: &Painter) {
+        for knot in &self.knots {
+            painter.circle_filled(knot.position.to_pos2(),
+            KNOT_RADIUS,
+            Color32::WHITE,
+            );
+        }
+    }
+
+    fn paint_segments(&self, painter: &Painter) {
+        for i in 0..self.knots.len()-1 {
+            painter.line_segment(
+                [
+                    self.knots[i].position.to_pos2(),
+                    self.knots[i+1].position.to_pos2(),
+                ],
+                Stroke {
+                    width: ROPE_THICKNESS,
+                    color: Color32::WHITE,
+                },
+            );
+        }
+    }
+
+    fn paint_rope(&self, painter: &Painter) {
+        self.paint_segments(painter);
+        self.paint_knots(painter);
     }
 }
 
@@ -162,59 +133,16 @@ impl eframe::App for MyApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             let painter = ui.painter();
 
-            let mut mouse = Pos2 { x: 0.0, y: 0.0 };
+            let mouse = ctx.input().pointer.hover_pos().unwrap_or(Pos2::default());
 
-            if let Some(mouse_position) = { ctx.input().pointer.hover_pos() } {
-                mouse = mouse_position;
-                if ctx.input().pointer.any_pressed() {
-                    for i in 0..self.knots.len() {
-                        let distance = (self.knots[i].position - mouse_position.to_vec2()).length();
-                        if distance <= KNOT_RADIUS {
-                            self.dragging = Some(i);
-                            break;
-                        }
-                    }
-                } else if ctx.input().pointer.any_released() {
-                    self.dragging = None;
-                }
-            }
+            self.knots[0].position = mouse.to_vec2();
 
-            for _ in 0..NUM_POSITION_PASSES {
-                for i in 0..self.knots.len() {
-                    let velocity = self.calculate_velocity_for(i);
-                    let dt_per_pass = ui.input().stable_dt.min(0.1) / NUM_POSITION_PASSES as f32;
-                    self.knots[i].position += velocity * TENSION * dt_per_pass;
+            self.satisfy_constraints();
 
-                    if let Some(dragged) = self.dragging {
-                        if dragged == i {
-                            self.knots[i].position = mouse.to_vec2();
-                        }
-                    }
-                }
-            }
+            self.knots[0].position = mouse.to_vec2();
 
-            for i in 1..self.knots.len() {
-                for n in &self.knots[i].neighbours {
-                    painter.line_segment(
-                        [
-                            self.knots[i].position.to_pos2(),
-                            self.knots[*n].position.to_pos2(),
-                        ],
-                        Stroke {
-                            width: ROPE_THICKNESS,
-                            color: Color32::GRAY,
-                        },
-                    );
-                }
-            }
-
-            for i in 0..self.knots.len() {
-                painter.circle_filled(
-                    self.knots[i].position.to_pos2(),
-                    KNOT_RADIUS,
-                    Color32::from_rgb(255 / (i + 1) as u8, 127, (i + 1) as u8),
-                );
-            }
+            self.paint_colliders(painter);
+            self.paint_rope(painter);
         });
 
         ctx.request_repaint_after(Duration::from_millis(5));
